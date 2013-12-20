727524b012064075b771ebe0c29fe7125b0a1307
world_engineer <alakazam442@yahoo.com>
2013-12-19 22:24:03 -0500

    initial commit
    Not much to see yet, general description of the application

---
+++ Dice_Roller.py
@@ -0,0 +1,7 @@
+'''Dice Roller - This dice roller takes a string of symbols, parses them into
+dice commands and then provides the result of the rolls. These commands
+ approximate various options in a number of gaming systems including features
+ such as critical hits, dice pools, and exploding criticals.
+
+ Copyright 2013 - Weston Hopkins'''
+
